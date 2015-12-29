4890910bd36b6ccff816e2df0d3288b3ec3fd14f
timkeane <tkeane@doitt.nyc.gov>
2015-12-29 13:20:08 -0500

    track tooltip changes

--- src/main/js/nyc/carto/popup.js
+++ src/main/js/nyc/carto/popup.js
@@ -20,13 +20,16 @@ nyc.carto.Popup = function(options){
 	layer.setInteractivity(interactivity);
 	this.tip(map, layer, tmpl);
 	layer.on('featureClick', $.proxy(this.captureClick, this)),
+	layer.on('featureOver', $.proxy(this.captureHover, this)),
 	this.infowin = cdb.vis.Vis.addInfowindow(map, layer, interactivity, {infowindowTemplate: tmpl});
 	this.infowin.model.set('sanitizeTemplate', false);
-	this.display = 'none';
-	this.onShow = options.onShow || this.onShow; 
-	this.onHide = options.onHide || this.onHide; 
+	this.popupDisplay = 'none';
+	this.tipDisplay = 'none';
+	this.onShowPopup = options.onShowPopup || this.onShowPopup; 
+	this.onHidePopup = options.onHidePopup || this.onHidePopup; 
+	this.onTipChange = options.onTipChange || this.onTipChange; 
 	$(this.infowin.el).css('z-index', 100);
-	this.observe();
+	this.observePopup(this);
 };
 
 nyc.carto.Popup.prototype = {
@@ -37,30 +40,98 @@ nyc.carto.Popup.prototype = {
 	 */
 	infowin: null,
 	/**
+	 * @desc The CartoDB tooltip
+	 * @public
+	 * @member {Object} 
+	 */
+	tip: null,
+	/**
 	 * @desc The CartoDB featureClick event arguments
 	 * @public
 	 * @member {Array<Object>} 
 	 */
-	eventData: null,
+	popupEventData: null,
+	/**
+	 * @desc The CartoDB featureOver event arguments
+	 * @public
+	 * @member {Array<Object>} 
+	 */
+	tipEventData: null,
 	/**
 	 * @private
 	 * @member {string}
 	 */
-	display: null,
+	popupDisplay: null,
+	/**
+	 * @private
+	 * @member {string}
+	 */
+	tipDisplay: null,
 	/**
 	 * @private
 	 * @method
 	 */
 	captureClick: function() {
-		this.eventData = arguments;
+		this.popupEventData = arguments;
 	},
 	/**
 	 * @private
 	 * @method
 	 */
-	observe: function(){
-		var me = this;
-		me.observer = new MutationObserver(function(mutations) {
+	captureHover: function() {
+		this.tipEventData = arguments;
+	},
+	/**
+	 * @private
+	 * @method
+	 */
+	tip: function(map, layer, tmpl){
+		var tipTmpl = $(tmpl).find('.tip-tmpl').html();
+		this.tip = map.viz.addOverlay({
+			type: 'tooltip',
+			layer: layer,
+			template: tipTmpl, 
+			position: 'bottom|right',
+			fields: [{}]
+        });
+        $(map.getContainer()).append(this.tip.render().el);
+        this.observeTip(this);
+	},
+	/**
+	 * @private
+	 * @method
+	 */
+	observeTip: function(me){
+		var observer = new MutationObserver(function(mutations) {
+			var display;
+			$.each(mutations, function(_, mutation) {
+				if (mutation.attributeName == 'style'){
+					display = mutation.target.style.top;
+					return false;
+				}
+			});
+			if (display && display != me.tipDisplay){
+				me.tipDisplayChanged(display);
+			}
+		});
+		observer.observe(me.tip.render().el, {attributes: true});	
+	},
+	/**
+	 * @private
+	 * @method
+	 * @param {string} display
+	 */
+	tipDisplayChanged: function(display){
+		this.tipDisplay = display;
+		this.onTipChange(this);
+		this.trigger('tipchange', this);
+	},
+	/**
+	 * @private
+	 * @method
+	 */
+	observePopup: function(me){
+		var observer = new MutationObserver(function(mutations) {
 			var display;
 			$.each(mutations, function(_, mutation) {
 				if (mutation.attributeName == 'style'){
@@ -68,54 +139,45 @@ nyc.carto.Popup.prototype = {
 					return false;
 				}
 			});
-			if (display && display != me.display){
-				me.displayChanged(display);
+			if (display && display != me.popupDisplay){
+				me.popupDisplayChanged(display);
 			}
 		});
-		me.observer.observe(this.infowin.el, {attributes: true});	
+		observer.observe(me.infowin.el, {attributes: true});	
 	},
 	/**
 	 * @private
 	 * @method
 	 * @param {string} display
 	 */
-	displayChanged: function(display){
-		this.display = display;
+	popupDisplayChanged: function(display){
+		this.popupDisplay = display;
 		if (display == 'block'){
-			this.trigger('show', this);
-			this.onShow(this);
+			this.onShowPopup(this);
+			this.trigger('showpopup', this);
 		}else{
-			this.trigger('hide', this);
-			this.onHide(this);
+			this.onHidePopup(this);
+			this.trigger('hidepopup', this);
 		}
 	},
 	/**
 	 * @private
 	 * @method
+	 * @param {nyc.carto.Popup} popup
 	 */
-	tip: function(map, layer, tmpl){
-		var tipTmpl = $(tmpl).find('.tip-tmpl').html(),
-			tooltip = map.viz.addOverlay({
-				type: 'tooltip',
-				layer: layer,
-				template: tipTmpl, 
-				position: 'bottom|right',
-				fields: [{}]
-	        });
-        $(map.getContainer()).append(tooltip.render().el);
-	},
+	onShowPopup: function(popup){},
 	/**
 	 * @private
 	 * @method
 	 * @param {nyc.carto.Popup} popup
 	 */
-	onShow: function(popup){},
+	onHidePopup: function(popup){},
 	/**
 	 * @private
 	 * @method
 	 * @param {nyc.carto.Popup} popup
 	 */
-	onHide: function(popup){}
+	onTipChange: function(popup){}
 };
 
 nyc.inherits(nyc.carto.Popup, nyc.EventHandling);
@@ -128,21 +190,25 @@ nyc.carto.Popup.EventType = {
 	/**
 	 * @desc The popup show event type
 	 */
-	SHOW: 'show',
+	SHOW_POPUP: 'showpopup',
 	/**
-	 * @desc The popup hide event types fired when a popup is opened or closed
+	 * @desc The popup hide event type
 	 */
-	HIDE: 'hide' 
+	HIDE_POPUP: 'hidepopup', 
+	/**
+	 * @desc The tooltip change event type
+	 */
+	TIP_CHANGE: 'tipchange'
 };
 
 /**
- * @desc The result of symbolization 
+ * @desc The result of a popup or tooltip show event 
  * @event nyc.carto.Popup#show
  * @type {nyc.carto.Popup}
  */
 
 /**
- * @desc The result of symbolization 
+ * @desc The result of a popup or tooltip hide event 
  * @event nyc.carto.Popup#hide
  * @type {nyc.carto.Popup}
  */
@@ -155,7 +221,8 @@ nyc.carto.Popup.EventType = {
  * @property {cartodb.Layer} layer The layer whose feature info will be displayed in the popup
  * @property {Array<string>} interactivity The data fields required by the template 
  * @property {string} template Template with replacement tokens for popup content (may optionally include an element of class 'tip-tmpl' for rendering tooltips)
- * @property {function(nyc.carto.Popup)=} onShow A function to handle the show event 
- * @property {function(nyc.carto.Popup)=} onHide A function to handle the hide event 
+ * @property {function(nyc.carto.Popup)=} onShowPopup A function to handle the popup show event 
+ * @property {function(nyc.carto.Popup)=} onHidePopup A function to handle the popup hide event 
+ * @property {function(nyc.carto.Popup)=} onTipChange A function to handle the tooltip change event 
  */
 nyc.carto.Popup.Options;
