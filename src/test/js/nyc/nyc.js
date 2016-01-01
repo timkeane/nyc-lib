f839666599c231ccf0adb3be1f1e19367da87bbc
timkeane <tkeane@doitt.nyc.gov>
2016-01-01 01:33:11 -0500

    formatNumberHtml tests

--- src/test/js/nyc/nyc.js
+++ src/test/js/nyc/nyc.js
@@ -216,3 +216,56 @@ QUnit.test('inherits', function(assert){
 	assert.deepEqual(parent.prototype.obj, {name:'object'});
 });
 
+QUnit.test('formatNumberHtml (default)', function(assert){
+	assert.expect(3);
+	var small = $('<div class="fmt-num">123</div>');
+	var million = $('<div class="fmt-num">1000000</div>');
+	var decimal = $('<div class="fmt-num">1234.1234567</div>');
+	$('body').append([small, million, decimal]);
+	
+	nyc.formatNumberHtml($('.fmt-num'));
+	
+	assert.equal(small.html(), '123');
+	assert.equal(million.html(), '1,000,000');
+	assert.equal(decimal.html(), '1,234.1235');
+
+	small.remove();
+	million.remove();
+	decimal.remove();
+});
+
+QUnit.test('formatNumberHtml (decimal)', function(assert){
+	assert.expect(3);
+	var small = $('<div class="fmt-num">123</div>');
+	var million = $('<div class="fmt-num">1000000</div>');
+	var decimal = $('<div class="fmt-num">1234.1234567</div>');
+	$('body').append([small, million, decimal]);
+	
+	nyc.formatNumberHtml($('.fmt-num'), {style:"decimal", minimumFractionDigits: 3});
+	
+	assert.equal(small.html(), '123.000');
+	assert.equal(million.html(), '1,000,000.000');
+	assert.equal(decimal.html(), '1,234.123');
+
+	small.remove();
+	million.remove();
+	decimal.remove();
+});
+
+QUnit.test('formatNumberHtml (currency)', function(assert){
+	assert.expect(3);
+	var small = $('<div class="fmt-num">123</div>');
+	var million = $('<div class="fmt-num">1000000</div>');
+	var decimal = $('<div class="fmt-num">1234.1234567</div>');
+	$('body').append([small, million, decimal]);
+	
+	nyc.formatNumberHtml($('.fmt-num'), {style:"currency", currency: 'USD'});
+	
+	assert.equal(small.html(), '$123.00');
+	assert.equal(million.html(), '$1,000,000.00');
+	assert.equal(decimal.html(), '$1,234.12');
+
+	small.remove();
+	million.remove();
+	decimal.remove();
+});
