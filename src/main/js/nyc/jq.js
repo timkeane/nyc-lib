5cec7f46f133b2ff1b7a99a2923a69a467b03cf1
timkeane <tkeane@doitt.nyc.gov>
2017-01-03 16:26:50 -0500

    don't allow jqueryui to load more than once... really only issue for qunit

--- src/main/js/nyc/jq.js
+++ src/main/js/nyc/jq.js
@@ -10,6 +10,12 @@ nyc.jq.ui = {
 	/**
 	 * @private
 	 * @static
+	 * @member {nyc.jq.ui.LoadState}
+	 */
+	loadState: 0,
+	/**
+	 * @private
+	 * @static
 	 * @member {string}
 	 */
 	js: 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js',
@@ -18,28 +24,47 @@ nyc.jq.ui = {
 	 * @public
 	 * @static
 	 * @method 
-	 * @param {function} callback A function to call once JQueryUI has loaded {@see https://api.jquery.com/jquery.getscript/}
+	 * @param {function()} callback A function to call once JQueryUI has loaded {@see https://api.jquery.com/jquery.getscript/}
 	 */
 	load: function(callback){
-		if (!$.ui || !$.ui.draggable){
+		if (nyc.jq.ui.loadState == 0 && (!$.ui || !$.ui.sortable)){
+			nyc.jq.ui.loadState = 1;
 			$.getScript(this.js, function(script, status, xhr){
+				nyc.jq.ui.loadState = 2;
 				/* Fix JQueryMobile widgets that will be broken by loading JQueryUI */
 				$('.nyc-choice input').checkboxradio({});
-				nyc.jq.ui.fixCss();
-				if (callback) callback(script, status, xhr);
+				nyc.jq.ui.fixCss(callback);
 			});
+		}else if (this.loadState == 1){
+			setTimeout(function(){
+				nyc.jq.ui.load(callback);
+			}, 100);
 		}else{
 			$('.nyc-choice input').checkboxradio({});
-			this.fixCss();			
-			if (callback) callback(script, status, xhr);
+			nyc.jq.ui.fixCss(callback);			
 		}
 	},
 	/**
 	 * @private
 	 * @static
 	 * @method 
+	 * @param {function()} callback
 	 */
-	fixCss: function(){
+	fixCss: function(callback){
 		$('.nyc-choice label.ui-radio-on').removeClass('ui-radio-on').addClass('ui-radio-off');		
+		if (callback){
+			callback();
+		}
 	}
+};
+
+/** 
+ * @desc Enumerator for popup event types
+ * @public
+ * @enum {number}
+ */
+nyc.jq.ui.LoadState = {
+	NOT_LOADED: 0,
+	LOADING: 1,
+	LOADED: 2
 };
