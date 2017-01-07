1894e2c529c8f15ac7bb23c5e381faa9f5717a89
timkeane <tkeane@doitt.nyc.gov>
2017-01-06 19:47:20 -0500

    ctlcontainer

---
+++ src/main/js/nyc/ctlcontainer.js
@@ -0,0 +1,32 @@
+var nyc = nyc || {};
+
+/**
+ * @desc  An abstract class for accessing a control container and elements within it
+ * @public
+ * @class
+ * @constructor
+ */
+nyc.CtlContainer = function(){};
+
+nyc.MapContainer.prototype = {
+	/**
+	 * @desc A method to return a control container HTML element wrapped in a JQuery
+	 * @public
+	 * @abstract
+	 * @method
+	 * @return {JQuery} The the control container HTML element wrapped in a JQuery
+	 */
+	getContainer: function(){
+		throw 'Must be implemented';
+	},
+	/**
+	 * @desc A method to return an element in the container
+	 * @public
+	 * @abstract
+	 * @method
+	 * @return {JQuery} The element
+	 */
+	getElem: function(selector){
+		return this.getContainer().find(selector);
+	}
+};
