1687556ceab708a1e5ac079043279df0388e0349
timkeane <tkeane@doitt.nyc.gov>
2018-04-25 10:31:14 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import _nyc from './nyc/nyc'
-import _nyc_Basemap from './nyc/Basemap'
+import _nyc_BasemapHelper from './nyc/BasemapHelper'
 import _nyc_EventHandling from './nyc/EventHandling'
 import _nyc_ReplaceTokens from './nyc/ReplaceTokens'
 
 window.nyc = _nyc
-window.nyc.Basemap = _nyc_Basemap
+window.nyc.BasemapHelper = _nyc_BasemapHelper
 window.nyc.EventHandling = _nyc_EventHandling
 window.nyc.ReplaceTokens = _nyc_ReplaceTokens
 
