0baf14a36b4968eec65f4b9008c39647bd279cd6
timkeane <tkeane@doitt.nyc.gov>
2018-05-05 14:28:42 -0400

    new container test

--- __tests__/nyc/Container.test.js
+++ __tests__/nyc/Container.test.js
@@ -6,6 +6,7 @@ import EventHandling from 'nyc/EventHandling'
 test('everything', () => {
   const parent = $('<div></div>')
   const child = $('<p></p>')
+  const append = $('<span></span>')
 
   parent.append(child)
   const container = new Container(parent)
@@ -14,4 +15,10 @@ test('everything', () => {
   expect(container instanceof EventHandling).toBe(true)
   expect(container.getContainer().get(0)).toBe(parent.get(0))
   expect(container.find('p').get(0)).toBe(child.get(0))
+  expect(parent.children().first().get(0)).toBe(child.get(0))
+
+  expect(container.append(append).get(0)).toBe(parent.get(0))
+  expect(container.find('span').get(0)).toBe(append.get(0))
+  expect(parent.children().last().get(0)).toBe(append.get(0))
+
 })
