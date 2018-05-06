e35e64ebe624d5a9d184afcb8c27c33acf8de3c6
timkeane <tkeane@doitt.nyc.gov>
2018-05-06 19:46:37 -0400

    csv, decorating, autoload

--- src/nyc/ol/source/AutoLoad.js
+++ src/nyc/ol/source/AutoLoad.js
@@ -8,12 +8,31 @@ import OlSourceVector from 'ol/source/vector'
 
 const fetch = window.fetch || require('node-fetch')
 
+/**
+ * @desc Class to auto load all features from a URL
+ * @public
+ * @class
+ * @extends {ol.source.Vector}
+ */
 export default class AutoLoad extends OlSourceVector {
+  /**
+   * @desc Creates an instance of AutoLoad
+   * @public
+   * @constructor
+   * @extends {ol.source.Vector}
+   * @param {Object} options
+   */
   constructor(options) {
     options.loader = () => {}
     super(options)
     this.autoLoadOptions = options
   }
+  /**
+   * @desc Load all features
+   * @public
+   * @method
+   * @return {Promise} features
+   */
   autoLoad() {
     const options = this.autoLoadOptions
     const format = this.getFormat()
