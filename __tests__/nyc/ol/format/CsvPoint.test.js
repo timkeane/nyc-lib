f5692c17351832a2bb63bed4405b8bf7d2073f08
timkeane <tkeane@doitt.nyc.gov>
2018-05-06 12:33:07 -0400

    csvpoint format

---
+++ __tests__/nyc/ol/format/CsvPoint.test.js
@@ -0,0 +1,83 @@
+import proj4 from 'proj4'
+
+import nyc from 'nyc/nyc'
+
+import CsvPoint from 'nyc/ol/format/CsvPoint'
+
+import OlFeaure from 'ol/feature'
+import OlFormatFeaure from 'ol/format/feature'
+import OlGeomPoint from 'ol/geom/point'
+
+const sourceId = [
+  {id: 'a', x: 0, y: 0, name: 'foo'},
+  {id: 'b', x: 1, y: 2, name: 'bar'},
+  {id: 'c', x: 2, y: 3, name: 'wtf'}
+]
+const sourceNoId = [
+  {x: 0, y: 0, name: 'foo'},
+  {x: 1, y: 2, name: 'bar'},
+  {x: 2, y: 3, name: 'wtf'}
+]
+
+test('readFeatures has id and projections', () => {
+  const csvpoint = new CsvPoint({
+    x: 'x',
+    y: 'y',
+    id: 'id',
+    defaultDataProjection: 'EPSG:2263',
+    defaultFeatureProjection: 'EPSG:4326'
+  })
+
+  const features = csvpoint.readFeatures(sourceId)
+
+  expect(features.length).toBe(3)
+  expect(features[0].getGeometry().getCoordinates()).toEqual(proj4('EPSG:2263', 'EPSG:4326', [0, 0]))
+  expect(features[1].getGeometry().getCoordinates()).toEqual(proj4('EPSG:2263', 'EPSG:4326', [1, 2]))
+  expect(features[2].getGeometry().getCoordinates()).toEqual(proj4('EPSG:2263', 'EPSG:4326', [2, 3]))
+
+  expect(features[0].getProperties().x).toBe(0)
+  expect(features[0].getProperties().y).toBe(0)
+  expect(features[0].getProperties().name).toBe('foo')
+
+  expect(features[1].getProperties().x).toBe(1)
+  expect(features[1].getProperties().y).toBe(2)
+  expect(features[1].getProperties().name).toBe('bar')
+
+  expect(features[2].getProperties().x).toBe(2)
+  expect(features[2].getProperties().y).toBe(3)
+  expect(features[2].getProperties().name).toBe('wtf')
+
+  expect(features[0].getId()).toBe('a')
+  expect(features[1].getId()).toBe('b')
+  expect(features[2].getId()).toBe('c')
+})
+
+test('readFeatures no id default projections', () => {
+  const csvpoint = new CsvPoint({
+    x: 'x',
+    y: 'y'
+  })
+
+  const features = csvpoint.readFeatures(sourceNoId)
+
+  expect(features.length).toBe(3)
+  expect(features[0].getGeometry().getCoordinates()).toEqual(proj4('EPSG:4326', 'EPSG:3857', [0, 0]))
+  expect(features[1].getGeometry().getCoordinates()).toEqual(proj4('EPSG:4326', 'EPSG:3857', [1, 2]))
+  expect(features[2].getGeometry().getCoordinates()).toEqual(proj4('EPSG:4326', 'EPSG:3857', [2, 3]))
+
+  expect(features[0].getProperties().x).toBe(0)
+  expect(features[0].getProperties().y).toBe(0)
+  expect(features[0].getProperties().name).toBe('foo')
+
+  expect(features[1].getProperties().x).toBe(1)
+  expect(features[1].getProperties().y).toBe(2)
+  expect(features[1].getProperties().name).toBe('bar')
+
+  expect(features[2].getProperties().x).toBe(2)
+  expect(features[2].getProperties().y).toBe(3)
+  expect(features[2].getProperties().name).toBe('wtf')
+
+  expect(features[0].getId()).toBe(0)
+  expect(features[1].getId()).toBe(1)
+  expect(features[2].getId()).toBe(2)
+})
---
+++ src/nyc/ol/format/CsvPoint.js
@@ -0,0 +1,103 @@
+/**
+ * @module nyc/ol/format/CsvPoint
+ */
+
+import proj4 from 'proj4'
+
+import nyc from 'nyc/nyc'
+
+import OlFeature from 'ol/feature'
+import OlFormatFeature from 'ol/format/feature'
+import OlGeomPoint from 'ol/geom/point'
+
+/**
+ * @desc Class to create point features from CSV data
+ * @public
+ * @class
+ */
+class CsvPoint extends OlFormatFeature {
+  constructor(options) {
+    super()
+    /**
+     * @private
+     * @member {ol.ProjectionLike}
+     */
+    this.defaultDataProjection = options.defaultDataProjection || 'EPSG:4326'
+    /**
+     * @private
+     * @member {ol.ProjectionLike}
+     */
+    this.defaultFeatureProjection = options.defaultFeatureProjection || 'EPSG:3857'
+    /**
+     * @private
+     * @member {string}
+     */
+    this.id = options.id
+    /**
+     * @private
+     * @member {string}
+     */
+    this.x = options.x
+    /**
+     * @private
+     * @member {string}
+     */
+    this.y = options.y
+    /**
+     * @private
+     * @member {number}
+     */
+    this.lastId = 0
+  }
+  /**
+   * @desc Read a single feature from a source
+   * @public
+   * @method
+   * @param {Object} source A row from a {@see nyc.ol.source.CsvPoint} data source
+   * @param {olx.format.ReadOptions=} options Read options
+   * @return {ol.Feature} Feature
+   */
+  readFeature(source, options) {
+    options = this.adaptOptions(options)
+    const feature = new OlFeature(source)
+    const coord = proj4(
+      options.dataProjection,
+      options.featureProjection,
+      [source[this.x], source[this.y]]
+    )
+    const point = new OlGeomPoint(coord)
+    const id = source[this.id] || this.lastId++
+    feature.setGeometry(point)
+    feature.setId(id)
+    return feature
+  }
+  /**
+   * @desc Read all features from a source
+   * @public
+   * @method
+   * @param {Array<Object>} source Rows from a {@see nyc.ol.source.CsvPoint} data source
+   * @param {olx.format.ReadOptions=} options Read options
+   * @return {Array.<ol.Feature>} Features
+   */
+  readFeatures(source, options) {
+    const features = []
+    source.forEach(row => {
+      features.push(this.readFeature(row, options))
+    })
+    return features
+  }
+}
+
+/**
+* @desc Constructor options for {@link Choice}
+* @public
+* @typedef {Object}
+* @property {string} x The name of the field containing the x ordinate of the point
+* @property {string} y The name of the field containing the y ordinate of the point
+* @property {string=} id The name of the field containing the unique id of the point
+* @property {ol.ProjectionLike} [defaultDataProjection=EPSG:4326] The projection of the source data
+* @property {ol.ProjectionLike} [defaultFeatureProjection=EPSG:3857] The projection of the resulting features
+*/
+CsvPoint.Options
+
+export default CsvPoint
