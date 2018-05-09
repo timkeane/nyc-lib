95fcb22c0a2ccc471ad1abcd23868d7221ff6981
timkeane <tkeane@doitt.nyc.gov>
2018-05-09 13:53:43 -0400

    remove debug code

--- src/nyc/ol/Popup.js
+++ src/nyc/ol/Popup.js
@@ -92,44 +92,24 @@ class Popup extends OlOverlay {
    */
   pan() {
     const popup = this.popup
-    console.warn(popup.css('display'));
     if (popup.css('display') !== 'none') {
       const view = this.map.getView()
       const tailHeight = parseInt(popup.css('bottom'))
-
-      console.warn('tailHeight',tailHeight)
-
       const tailOffsetLeft = -parseInt(popup.css('left'))
       const popOffset = this.getOffset()
       const popPx = this.map.getPixelFromCoordinate(this.getPosition())
-
-      console.warn('popPx', popPx);
-
       const mapSize = this.map.getSize()
       const popSize = {
         width: popup.width(),
         height: popup.height() + tailHeight
       }
       const tailOffsetRight = popSize.width - tailOffsetLeft
-
-      console.warn('tailOffsetLeft', tailOffsetLeft);
-      console.warn('popSize', popSize);
-      console.warn('tailOffsetRight', tailOffsetRight);
-
       const fromLeft = (popPx[0] - tailOffsetLeft) - this.margin[3]
       const fromRight = mapSize[0] - (popPx[0] + tailOffsetRight) - this.margin[1]
       const fromTop = popPx[1] - popSize.height + popOffset[1] - this.margin[0]
       const fromBottom = mapSize[1] - (popPx[1] + tailHeight) - popOffset[1] - this.margin[2]
       const center = view.getCenter()
       const px = this.map.getPixelFromCoordinate(center)
-
-      console.warn('fromRight',fromRight);
-      console.warn('fromLeft',fromLeft);
-      console.warn('fromTop',fromTop);
-      console.warn('fromBottom',fromBottom);
-
-      console.error('px before', px)
-
       if (fromRight < 0) {
         px[0] -= fromRight
       } else if (fromLeft < 0) {
@@ -140,9 +120,6 @@ class Popup extends OlOverlay {
       } else if (fromBottom < 0) {
         px[1] -= fromBottom
       }
-
-      console.error('px after', px)
-
       view.animate({center: this.map.getCoordinateFromPixel(px)})
     }
   }
