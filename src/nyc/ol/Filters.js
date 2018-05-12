510efc201c214eb8d947af8ba89ce6a688ceb335
timkeane <tkeane@doitt.nyc.gov>
2018-05-12 11:03:42 -0400

    filters container

---
+++ src/nyc/ol/Filters.js
@@ -0,0 +1,48 @@
+/**
+ * @module nyc/Filters
+ */
+
+import Container from 'nyc/Container'
+import Choice from 'nyc/Choice'
+
+class Filters extends Container {
+  constructor(options) {
+    super(options.target)
+    this.filterControls = []
+    this.source = options.source
+    options.filterControlOptions.forEach((option, i) => {
+      option.target = option.target || $(`<div class="${nyc.nextId('filter')}"></div>`)
+      this.append($(option.target))
+      const choice = new Choice(option)
+      choice.on('change', $.proxy(this.filter, this))
+      this.filterControls.push(choice)
+    })
+  }
+  filter() {
+    const namedFilters = {}
+    const filters = []
+    this.filterControls.forEach(filterControl => {
+      filterControl.val().forEach(choice => {
+        const filter = namedFilters[choice.name] || []
+        namedFilters[choice.name] = filter.concat(choice.values)
+      })
+    })
+    Object.keys(namedFilters).forEach(name => {
+      filters.push({property: name, values: namedFilters[name]})
+    })
+    this.source.filter(filters)
+    this.trigger('change', this)
+  }
+}
+
+/**
+ * @desc Object type to hold constructor options for
+ * @public
+ * @typedef {Object}
+ * @property {JQuery|Element|string} target
+ * @property {Array<Choice.Options>} filterControlOptions
+ * @property {nyc.ol.source.FilterAndSort} source The source to filter
+ */
+Filters.Options
+
+export default Filters
