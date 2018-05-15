11bd2383733582fbfeae87eebfe723bf4eed9d60
timkeane <tkeane@doitt.nyc.gov>
2018-05-15 14:02:29 -0400

    break it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import wtf from 'wtf'
+
 import $ from 'jquery'
 
 import _nyc from 'nyc/nyc'
@@ -91,4 +93,3 @@ import _nyc_leaf_Basemap from 'nyc/leaf/Basemap'
 
 window.nyc.leaf = {}
 window.nyc.leaf.Basemap = _nyc_leaf_Basemap
-x.x
