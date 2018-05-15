8d010a8e6763bdb388c44c1067a685be3b62c309
timkeane <tkeane@doitt.nyc.gov>
2018-05-15 12:24:54 -0400

    leaflet basemap underway

--- __tests__/nyc/leaf/Basemap.test.js
+++ __tests__/nyc/leaf/Basemap.test.js
@@ -1,5 +1,6 @@
 import $ from 'jquery'
 
+import BasemapHelper from 'nyc/BasemapHelper'
 import Basemap from 'nyc/leaf/Basemap'
 
 import L from 'leaflet'
@@ -17,10 +18,33 @@ afterEach(() => {
 })
 
 test('constructor', () => {
+  expect.assertions(255)
+
   const basemap = new Basemap({target: 'map0'})
   const lMap = L.map('map1')
-  expect(basemap instanceof Basemap)
-  expect(typeof basemap.setView).toBe('function')
-  
-  // expect(Object.getOwnPropertyNames(basemap)).toEqual(Object.getOwnPropertyNames(lMap))
+  expect(basemap instanceof L.Map).toBe(true)
+
+  Object.keys(L.Map.prototype).forEach(member => {
+    expect(basemap[member]).not.toBe(undefined)
+    if (typeof L.Map.prototype[member] === 'function') {
+      expect(typeof basemap[member]).toBe('function')      
+    } else if (typeof L.Map.prototype[member] === 'object') {
+      expect(typeof basemap[member]).toBe('object')      
+    } else {
+      console.warn(`Untested member type ${typeof L.Map.prototype[member]}`)
+      throw 'FAIL!'
+    }
+  })
+
+  Object.keys(BasemapHelper).forEach(member => {
+    expect(basemap[member]).not.toBe(undefined)
+    if (typeof BasemapHelper[member] === 'function') {
+      expect(typeof basemap[member]).toBe('function')      
+    } else if (typeof BasemapHelper[member] === 'object') {
+      expect(typeof basemap[member]).toBe('object')      
+    } else {
+      console.warn(`Untested member type ${typeof BasemapHelper[member]}`)
+      throw 'FAIL!'
+    }
+  })
 })
--- src/nyc/leaf/Basemap.js
+++ src/nyc/leaf/Basemap.js
@@ -22,174 +22,91 @@ import L from 'leaflet'
 
 class Basemap {
 
-  constructor(props) {
-    const map = L.map(props.target)
-    map.storage = null
-
-    map.base = L.tileLayer(Basemap.BASE_URL, {
-      minNativeZoom: 8,
-      maxNativeZoom: 21,
-      subdomains: '1234',
-      tms: true,
-      bounds: Basemap.UNIVERSE_EXTENT,
-      zIndex: 0
-    })
-
-    map.base.name = 'base'
-
-    map.addLayer(map.base)
-
-		map.labels = {}
-		
-		Object.keys(Basemap.LABEL_URLS).map(labelType => {
-			map.labels[labelType] = L.tileLayer(Basemap.LABEL_URLS[labelType], {
-        minNativeZoom: 8,
-        maxNativeZoom: 21,
-        subdomains: '1234',
-        tms: true,
-        bounds: Basemap.LABEL_EXTENT,
-        zIndex: 1000
-      })
-      map.labels[labelType].name = labelType
-      if (labelType == 'base') {
-        map.addLayer(map.labels[labelType])
-      }
-		})
-
-		map.photos = {}
-
-		Object.keys(Basemap.PHOTO_URLS).map(year => {
-			const photo = L.tileLayer(Basemap.PHOTO_URLS[year], {
-        minNativeZoom: 8,
-        maxNativeZoom: 21,
-        subdomains: '1234',
-        tms: true,
-        bounds: Basemap.PHOTO_EXTENT,
-        zIndex: 1
-      })
-      if ((year * 1) > map.latestPhoto) {
-        map.latestPhoto = year
-      }
-      photo.name = year
-      map.photos[year] = photo
-		})
-
-		map.fitBounds(Basemap.EXTENT)
-		
-		// /**
-		//  * @public
-		//  * @member {L.Map}
-		//  */
-		// this.map
-		// /**
-		//  * @private
-		//  * @member {L.TileLayer}
-		//  */
-		// this.base
-		// /**
-		//  * @private
-		//  * @member {Object<string, L.TileLayer>}
-		//  */
-		// this.labels = null
-		// /**
-		//  * @private
-		//  * @member {Object<string, L.TileLayer>}
-		//  */
-		// this.photos = null,
-		// /**
-		//  * @private
-		//  * @member {number}
-		//  */
-		// this.latestPhoto = 0,
-		// /**
-		//  * @private
-		//  * @member {nyc.leaf.storage.Local}
-		//  */
-		// this.storage = null
+  constructor(options) {
+    const map = L.map(options.target)
+		nyc.mixin(map, [BasemapHelper])
+    /**
+  	 * @private
+  	 * @member {number}
+  	 */
+  	map.latestPhoto = 0
+    /**
+  	 * @private
+  	 * @member {OlLayerTile}
+  	 */
+  	map.base = null
+    /**
+  	 * @private
+  	 * @member {Object<string, OlLayerTile>}
+  	 */
+  	map.labels = {}
+    /**
+  	 * @private
+  	 * @member {Object<string, OlLayerTile>}
+  	 */
+  	map.photos = {}
+    /**
+  	 * @private
+  	 * @member {storage.Local}
+  	 */
+    map.storage = null //new LocalStorage()
+    this.setupLayers(map, options)
+		map.fitBounds(Basemap.EXTENT);
+    map.hookupEvents(map.getContainer())
 
     return map
   }
-
-
   /**
-	 * @desc Get the storage used for laoding and saving data
-	 * @public
-	 * @override
-	 * @method
-	 * @return {nyc.leaf.LocalStorage} srorage
-	 */
-	getStorage(year) {
-		return this.storage
-	}
-	/**
-	 * @desc Show photo layer
-	 * @public
-	 * @override
-	 * @method
-	 * @param layer {number} The photo year to show
-	 */
-	showPhoto(year) {
-		year = year || this.latestPhoto
-		this.hidePhoto()
-		this.addLayer(this.photos[year + ''])
-		this.showLabels('photo')
-	}
-	/**
-	 * @desc Show photo layer
-	 * @public
-	 * @override
-	 * @method
-	 * @param labelType {nyc.Basemap.LabelType} The label type to show
-	 */
-	showLabels(labelType) {
-		this[labelType == Basemap.LabelType.BASE ? 'addLayer' : 'removeLayer'](this.labels.base)
-		this[labelType == Basemap.LabelType.PHOTO ? 'addLayer' : 'removeLayer'](this.labels.photo)
-	}
-	/**
-	 * @desc Hide photo layer
-	 * @public
-	 * @override
-	 * @method
-	 */
-	hidePhoto() {
-		this.showLabels(nyc.Basemap.LabelType.BASE)
-		for (var year in this.photos){
-			if (this.hasLayer(this.photos[year])){
-				this.removeLayer(this.photos[year])
+   * @private
+   * @method
+   * @param {L.Map} map
+   * @param {Object} options
+   */
+  setupLayers(map, options) {
+    map.base = L.tileLayer(Basemap.BASE_URL, {
+			minNativeZoom: 8,
+			maxNativeZoom: 21,
+			subdomains: '1234',
+			tms: true,
+			bounds: Basemap.UNIVERSE_EXTENT,
+			zIndex: 0
+		})
+		map.base.name = 'base'
+		map.addLayer(map.base)
+
+    Object.entries(Basemap.LABEL_URLS).forEach(([labelType, url]) => {
+      map.labels[labelType] = L.tileLayer(Basemap.LABEL_URLS[labelType], {
+				minNativeZoom: 8,
+				maxNativeZoom: 21,
+				subdomains: '1234',
+				tms: true,
+				bounds: Basemap.LABEL_EXTENT,
+				zIndex: 1000
+			})
+			map.labels[labelType].name = labelType
+			if (labelType == 'base') {
+				map.addLayer(map.labels[labelType])
 			}
-		}
-	}
-	/**
-	 * @desc Returns the base layers
-	 * @public
-	 * @override
-	 * @method
-	 * @return {nyc.Basemap.BaseLayers}
-	 */
-	getBaseLayers() {
-		return {
-			base: this.base,
-			labels: this.labels,
-			photos: this.photos
-		}
-	}
-	/**
-	 * @private
-	 * @method
-	 */
-	photoChange() {
-		for (var photo in this.photos){
-			if (this.hasLayer(this.photos[photo])){
-				this.showLabels(nyc.Basemap.LabelType.PHOTO)
-				return
+    })
+
+    Object.entries(Basemap.PHOTO_URLS).forEach(([year, url]) => {
+      const photo = L.tileLayer(url, {
+				minNativeZoom: 8,
+				maxNativeZoom: 21,
+				subdomains: '1234',
+				tms: true,
+				bounds: Basemap.PHOTO_EXTENT,
+				zIndex: 1
+			})
+			if ((year.split('-')[0] * 1) > this.latestPhoto) {
+				map.latestPhoto = year
 			}
-		}
-		this.showLabels(nyc.Basemap.LabelType.BASE)
-	}
+			photo.name = year;
+			map.photos[year] = photo;
+			map.photos[year] = photo
+    })
+  }
 }
-
-export default Basemap
-
 
 /**
  * @desc The URL of the New York City base map tiles
@@ -265,3 +182,5 @@ Basemap.LABEL_EXTENT = L.latLngBounds([40.0341, -74.2727], [41.2919, -71.9101])
  * @type {L.LatLngBounds}
  */
 Basemap.PHOTO_EXTENT = L.latLngBounds([40.4888, -74.2759], [40.9279, -73.6896])
+
+export default Basemap
