1d41cdb256dc76f61781697d1476e9a20b192b08
timkeane <tkeane@doitt.nyc.gov>
2018-05-21 21:59:34 -0400

    css build

--- conf/postcss.config.js
+++ conf/postcss.config.js
@@ -1,7 +1,12 @@
-module.exports = {
-  plugins: [
+const isProd = ['production', 'prod', 'prd'].indexOf(process.env.NODE_ENV) > -1
+let plugins = []
+if (isProd) {
+  plugins = [
     require('postcss-import')({}),
     require('postcss-css-variables')({}),
     require('postcss-clean')({})
   ]
+}
+module.exports = {
+  plugins: plugins
 }
