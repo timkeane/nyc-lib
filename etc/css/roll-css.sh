39c316354e066630e9947cb7ec64beb03c4f649a
timkeane <tkeane@doitt.nyc.gov>
2018-06-11 21:11:48 -0400

    .

--- etc/css/roll-css.sh
+++ etc/css/roll-css.sh
@@ -9,8 +9,8 @@ cp $2 $DIR/tmp/
 theme=$(basename $1)
 other=$(basename $2)
 cat $DIR/tmp/$theme >> $DIR/tmp/base.theme.css
-cat $DIR/tmp/$other >> $DIR/tmp/nyc-lib.css
 cat $DIR/tmp/$other >> $DIR/tmp/nyc-ol-lib.css
-postcss $DIR/tmp/nyc-lib.css --o $DIR/build/nyc.$theme --no-map --config $DIR
 postcss $DIR/tmp/nyc-ol-lib.css --o $DIR/build/nyc.ol.$theme --no-map --config $DIR
+cat $DIR/tmp/$other >> $DIR/tmp/nyc-lib.css
+postcss $DIR/tmp/nyc-lib.css --o $DIR/build/nyc.$theme --no-map --config $DIR
 rm -rf $DIR/tmp
