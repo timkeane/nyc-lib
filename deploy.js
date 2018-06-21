fb44bf58805adaa3e960ff2097d2a852baa9a534
timkeane <tkeane@doitt.nyc.gov>
2018-06-21 15:03:33 -0400

    zip

--- deploy.js
+++ deploy.js
@@ -1,48 +1,58 @@
 require('dotenv').config()
 const nodeEnv = process.env.NODE_ENV
 const isProd = ['production', 'prod', 'prd'].indexOf(nodeEnv) > -1
-const Client = require('ssh2').Client
 const package = require('./package.json')
 const name = package.name
 const version = package.version
+
+const Client = require('ssh2').Client
+const zip = require('zip-folder')
+
+const archiveFile = `${name}-v${version}.zip`
 const archiveDir = `${process.env.DEPLOY_DIR}/${name}/archive/`
 const deployDir = `${process.env.DEPLOY_DIR}/${name}/${version}/`
 
-const host = isProd ? process.env.PRD_SSH_HOST : process.env.STG_SSH_HOST
-const user = isProd ? process.env.PRD_SSH_USER : process.env.STG_SSH_USER
-const keyPath = process.env.SSH_KEY_PATH
+console.warn(archiveFile);
 
-console.warn(user,host,keyPath)
-
-const callback = function(err, stream) {
-  if (err) throw err
-  stream.on('close', function(code, signal) {
-    console.log('Stream :: close :: code: ' + code + ', signal: ' + signal)
-    conn.end()
-  })
-}
-
-const conn = new Client()
-
-conn.on('connect', () => {
-  console.log('- connected')
-})
-conn.on('error', (error) => {
-  console.log(`- conection error: ${error}`)
-})
-conn.on('end', () => {
-  console.log('done!')
-  process.exit(0)
+zip('dist', `./${archiveFile}`, (err) => {
+  if (err) {
+      console.log('zip error', err)
+      process.exit(1)
+  } else {
+      console.log('EXCELLENT')
+  }
 })
 
-conn.on('ready', function() {
-  conn.exec(`mkdir -p ${archiveDir}`, callback)
-  conn.exec(`mkdir -p ${deployDir}`, callback)
-})
+// const callback = function(err, stream) {
+//   if (err) throw err
+//   stream.on('close', function(code, signal) {
+//     console.log('Stream :: close :: code: ' + code + ', signal: ' + signal)
+//     conn.end()
+//   })
+// }
 
-conn.connect({
-  host: host,
-  username: user,
-  privateKey: require('fs').readFileSync(keyPath),
-  port: 22
-})
+// const conn = new Client()
+
+// conn.on('connect', () => {
+//   console.log('- connected')
+// })
+// conn.on('error', (error) => {
+//   console.log(`- conection error: ${error}`)
+//   process.exit(1)
+// })
+// conn.on('end', () => {
+//   console.log('done!')
+//   process.exit(0)
+// })
+
+// conn.on('ready', function() {
+//   conn.exec(`mkdir -p ${archiveDir}`, callback)
+//   conn.exec(`mkdir -p ${deployDir}`, callback)
+// })
+
+// conn.connect({
+//   host: isProd ? process.env.PRD_SSH_HOST : process.env.STG_SSH_HOST,
+//   username: process.env.SSH_USER,
+//   privateKey: require('fs').readFileSync(process.env.SSH_KEY_PATH),
+//   port: 22
+// })
