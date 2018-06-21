0224ed547650b274cab0f6b23f61ca0d4cbda2ed
timkeane <tkeane@doitt.nyc.gov>
2018-06-21 16:45:43 -0400

    still trying to figure out deploy script

--- deploy.js
+++ deploy.js
@@ -1,58 +1,78 @@
-require('dotenv').config()
-const nodeEnv = process.env.NODE_ENV
-const isProd = ['production', 'prod', 'prd'].indexOf(nodeEnv) > -1
 const package = require('./package.json')
 const name = package.name
-const version = package.version
+const version = `v${package.version}`
 
-const Client = require('ssh2').Client
 const zip = require('zip-folder')
-
-const archiveFile = `${name}-v${version}.zip`
-const archiveDir = `${process.env.DEPLOY_DIR}/${name}/archive/`
-const deployDir = `${process.env.DEPLOY_DIR}/${name}/${version}/`
-
-console.warn(archiveFile);
+const archiveFile = `${name}-${version}.zip`
 
 zip('dist', `./${archiveFile}`, (err) => {
   if (err) {
       console.log('zip error', err)
       process.exit(1)
   } else {
-      console.log('EXCELLENT')
+    deploy()
   }
 })
 
-// const callback = function(err, stream) {
-//   if (err) throw err
-//   stream.on('close', function(code, signal) {
-//     console.log('Stream :: close :: code: ' + code + ', signal: ' + signal)
-//     conn.end()
-//   })
-// }
+const deploy = () => {
+  require('dotenv').config()
+  const nodeEnv = process.env.NODE_ENV
+  const isProd = ['production', 'prod', 'prd'].indexOf(nodeEnv) > -1
 
-// const conn = new Client()
+  const Client = require('ssh2').Client
+  const fs = require('fs')
+  const archiveDir = `${process.env.DEPLOY_DIR}/${name}/archive`
+  const deployDir = `${process.env.DEPLOY_DIR}/${name}/${version}`
 
-// conn.on('connect', () => {
-//   console.log('- connected')
-// })
-// conn.on('error', (error) => {
-//   console.log(`- conection error: ${error}`)
-//   process.exit(1)
-// })
-// conn.on('end', () => {
-//   console.log('done!')
-//   process.exit(0)
-// })
-
-// conn.on('ready', function() {
-//   conn.exec(`mkdir -p ${archiveDir}`, callback)
-//   conn.exec(`mkdir -p ${deployDir}`, callback)
-// })
-
-// conn.connect({
-//   host: isProd ? process.env.PRD_SSH_HOST : process.env.STG_SSH_HOST,
-//   username: process.env.SSH_USER,
-//   privateKey: require('fs').readFileSync(process.env.SSH_KEY_PATH),
-//   port: 22
-// })
+  const callback = function(err, stream) {
+    if (err) throw err
+    stream.on('close', function(code, signal) {
+      console.log('Stream :: close :: code: ' + code + ', signal: ' + signal)
+      conn.end()
+    })
+  }
+  
+  const conn = new Client()
+  
+  conn.on('connect', () => {
+    console.log('- connected')
+  })
+  conn.on('error', (error) => {
+    console.log(`- conection error: ${error}`)
+    process.exit(1)
+  })
+  conn.on('end', () => {
+    console.log('done!')
+    process.exit(0)
+  })
+  
+  conn.on('ready', function() {
+    console.warn(`mkdir -p ${archiveDir}`);
+    console.warn(`mkdir -p ${deployDir}`);
+    conn.exec(`mkdir -p ${archiveDir}`, callback)
+    conn.exec(`mkdir -p ${deployDir}`, callback)
+    conn.sftp((err, sftp) => {
+      const read = fs.createReadStream(archiveFile)
+      const write = sftp.createWriteStream(`${archiveDir}/${archiveFile}`)
+      write.on('close', () => {
+        console.log(archiveFile, 'transferred to', `${archiveDir}/${archiveFile}`)
+        sftp.end()
+        conn.exec(`cp -R ${deployDir} ${deployDir}.bak`, callback)
+        conn.exec(`rm -rf ${deployDir}`, callback)
+        conn.exec(`unzip ${archiveDir}/${archiveFile} -d ${deployDir}`, callback)
+        conn.exec(`rm -rf ${deployDir}.bak`, (err, stream) => {
+          callback(err, stream)
+          conn.end()
+        })
+      })
+      read.pipe(write)
+    })
+  })
+  
+  conn.connect({
+    host: isProd ? process.env.PRD_SSH_HOST : process.env.STG_SSH_HOST,
+    username: process.env.SSH_USER,
+    privateKey: fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`),
+    port: 22
+  })
+}
