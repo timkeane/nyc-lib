7119ad089b79908c12027e1da9d27396e46894fc
timkeane <tkeane@doitt.nyc.gov>
2018-08-26 20:15:42 -0400

    v1.1.83

--- etc/css/roll-css.sh
+++ etc/css/roll-css.sh
@@ -9,15 +9,16 @@ cp $1 $DIR/tmp/
 cp $2 $DIR/tmp/
 theme=$(basename $1)
 other=$(basename $2)
-echo $full
-if [ $full != null ]; then
-  full="nyc-ol-lib.css"
-else
-  full="nyc-ol-fullscreen-lib.css"
-fi
+
 cat $DIR/tmp/$theme >> $DIR/tmp/base.theme.css
+
+cat $DIR/tmp/$other >> $DIR/tmp/nyc-ol-fullscreen-lib.css
+postcss $DIR/tmp/nyc-ol-fullscreen-lib.css --o $DIR/build/nyc.ol.fullscreen.$theme --no-map --config $DIR
+
 cat $DIR/tmp/$other >> $DIR/tmp/nyc-ol-lib.css
 postcss $DIR/tmp/nyc-ol-lib.css --o $DIR/build/nyc.ol.$theme --no-map --config $DIR
+
 cat $DIR/tmp/$other >> $DIR/tmp/nyc-lib.css
 postcss $DIR/tmp/nyc-lib.css --o $DIR/build/nyc.$theme --no-map --config $DIR
+
 rm -rf $DIR/tmp
