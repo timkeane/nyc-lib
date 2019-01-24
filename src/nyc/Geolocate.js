92ae9cc9043d5631c1ad7867b70d4d96a0f6188e
timkeane <tkeane@doitt.nyc.gov>
2019-01-24 15:13:28 -0500

    adjust for nyc ux team

--- src/nyc/Geolocate.js
+++ src/nyc/Geolocate.js
@@ -47,7 +47,7 @@ class Geolocate extends Container {
  * @type {string}
  */
 Geolocate.HTML = '<div class="geoloc">' +
-  '<button class="btn-geo btn-sq rad-all" title="Current location">' +
+  '<button class="btn btn-geo btn-sq rad-all btn-lg btn-dark" title="Current location">' +
     '<span class="screen-reader-only">Current location</span>' +
     '<span class="fas fa-location-arrow" role="img"></span>' +
   '</button>' +
