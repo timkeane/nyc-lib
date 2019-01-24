f6a3f3b98230c30e5bdaf8ee3035156133b336ca
timkeane <tkeane@doitt.nyc.gov>
2019-01-24 08:03:28 -0500

    ol locationmgr tests

--- __tests__/nyc/ol/LocationMgr.test.js
+++ __tests__/nyc/ol/LocationMgr.test.js
@@ -8,21 +8,23 @@ import NycOlZoom from 'nyc/ol/Zoom'
 import NycOlGeolocate from 'nyc/ol/Geolocate'
 import NycOlSearch from 'nyc/ol/Search'
 
-test('FIX ME', () => {})
 
-/*
+let div
 let target
 beforeEach(() => {
-  target = $('<div id="map"></div>')
+  target = $('<div id="search"></div>')
+  div = $('<div id="map"></div>')
   $('body').append(target)
+  $('body').append(div)
 })
 
 afterEach(() => {
   target.remove()
+  div.remove()
 })
 
-test('constructor', () => {
-  expect.assertions(7)
+test('constructor no search target', () => {
+  expect.assertions(11)
   
   const locationMgr = new LocationMgr({
     map: new Basemap({target: 'map'}),
@@ -31,10 +33,37 @@ test('constructor', () => {
 
   expect(locationMgr instanceof LocationMgr).toBe(true)
   expect(locationMgr instanceof NycLocationMgr).toBe(true)
-  expect(locationMgr.zoomSearch instanceof NycOlZoomSearch).toBe(true)
+  expect(locationMgr.zoom instanceof NycOlZoom).toBe(true)
+  expect(locationMgr.geolocate instanceof NycOlGeolocate).toBe(true)
+  expect(locationMgr.search instanceof NycOlSearch).toBe(true)
   expect(locationMgr.locator instanceof NycOlLocator).toBe(true)
   expect(locationMgr.locator.geocoder instanceof NycGeoclient).toBe(true)
   expect(locationMgr.locator.geocoder.url).toBe('http://geoclient.url?keys=keys&input=')
   expect(locationMgr.mapLocator instanceof NycOlMapLocator).toBe(true)
+
+  expect(div.find('.srch-ctr').length).toBe(1)
+  expect(locationMgr.search.getContainer().get(0)).toBe(div.find('.srch-ctr').get(0))
 })
-*/
+
+test('constructor with search target', () => {
+  expect.assertions(11)
+  
+  const locationMgr = new LocationMgr({
+    map: new Basemap({target: 'map'}),
+    searchTarget: target,
+    url: 'http://geoclient.url?keys=keys'
+  })
+
+  expect(locationMgr instanceof LocationMgr).toBe(true)
+  expect(locationMgr instanceof NycLocationMgr).toBe(true)
+  expect(locationMgr.zoom instanceof NycOlZoom).toBe(true)
+  expect(locationMgr.geolocate instanceof NycOlGeolocate).toBe(true)
+  expect(locationMgr.search instanceof NycOlSearch).toBe(true)
+  expect(locationMgr.locator instanceof NycOlLocator).toBe(true)
+  expect(locationMgr.locator.geocoder instanceof NycGeoclient).toBe(true)
+  expect(locationMgr.locator.geocoder.url).toBe('http://geoclient.url?keys=keys&input=')
+  expect(locationMgr.mapLocator instanceof NycOlMapLocator).toBe(true)
+
+  expect(target.find('.srch-ctr').length).toBe(1)
+  expect(locationMgr.search.getContainer().get(0)).toBe(target.find('.srch-ctr').get(0))
+})
