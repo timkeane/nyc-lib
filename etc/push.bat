f96157007048721004c0df0ff58efe1add04c0b6
timkeane <tkeane@doitt.nyc.gov>
2019-05-16 14:24:30 -0400

    .

--- etc/push.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\Windows Media Player\wmplayer" $1
--- etc/push.sh
+++ etc/push.sh
@@ -1,5 +1,5 @@
 echo 'Push it...'
-./winpush.sh
+./winpush.sh &>/dev/null &
 afplay nowpushit.wav &>/dev/null &
 echo 'Push it real good!'
 exit 0
