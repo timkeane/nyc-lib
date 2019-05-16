525adbde5877d5a92b9c5b8908a3a349cd612f2d
timkeane <tkeane@doitt.nyc.gov>
2019-05-16 14:07:06 -0400

    .

--- etc/push.sh
+++ etc/push.sh
@@ -1,5 +1,4 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-"C:\Program Files (x86)\Windows Media Player\wmplayer" "$DIR/nowpushit.wav" & 
-afplay nowpushit.wav
+"C:\Program Files (x86)\Windows Media Player\wmplayer" "$DIR/nowpushit.wav" &>/dev/null || afplay nowpushit.wav
 echo 'Push it real good!'
 exit 0
