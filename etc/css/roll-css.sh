73e2cd60294deb71d36a595b12846583b340b97c
timkeane <t.d.keane@gmail.com>
2022-07-15 10:20:51 -0400

    pass NODE_ENV to roll-css.sh

--- etc/css/roll-css.sh
+++ etc/css/roll-css.sh
@@ -9,20 +9,22 @@ cp $1 $DIR/tmp/
 cp $2 $DIR/tmp/
 theme=$(basename $1)
 other=$(basename $2)
+NODE_ENV=$2
 
 cat $DIR/tmp/$theme >> $DIR/tmp/base.theme.css
-
 cat $DIR/tmp/$other >> $DIR/tmp/nyc-ol-fullscreen-lib.css
+
 postcss $DIR/tmp/nyc-ol-fullscreen-lib.css --o $DIR/build/nyc.ol.fullscreen.$theme --no-map --config $DIR
 
 cat $DIR/tmp/$other >> $DIR/tmp/nyc-ol-lib.css
+
 postcss $DIR/tmp/nyc-ol-lib.css --o $DIR/build/nyc.ol.$theme --no-map --config $DIR
 
 cat $DIR/tmp/$other >> $DIR/tmp/nyc-lib.css
+
 postcss $DIR/tmp/nyc-lib.css --o $DIR/build/nyc.$theme --no-map --config $DIR
 
 if [ "$NODE_ENV" == "dev" ] || [ "$NODE_ENV" == "development" ]; then
   cp $DIR/tmp/* $DIR/build/
 fi
-
 rm -rf $DIR/tmp
